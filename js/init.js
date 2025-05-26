c731ec910523abd073a7976930838e712fda1d25
Gh8ster <98288402+Gh8ster@users.noreply.github.com>
2025-05-26 23:48:38 +0200

    Add files via upload

--- js/init.js
+++ js/init.js
@@ -1,1 +1,7 @@
-
+for (let i = 0; i !== 0; i++){
+    if (JSON.parse(localStorage.getItem(`saveGame${i+1}`)) !== null) {
+        files[i] = JSON.parse(localStorage.getItem(`saveGame${i+1}`));
+    } else {
+        i = 0
+    }
+}
